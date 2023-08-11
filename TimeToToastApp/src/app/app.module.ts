ea8bb92750ae539236b019d37604176de545d5f6
MaddoxGraham <57898176+MaddoxGraham@users.noreply.github.com>
2023-08-11 16:01:50 +0200

    front login

--- TimeToToastApp/src/app/app.module.ts
+++ TimeToToastApp/src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './View/landing-page/landing-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthContentComponent } from './auth-content/auth-content.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ContentComponent } from './content/content.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    AuthContentComponent
+    AuthContentComponent,
+    LoginFormComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
